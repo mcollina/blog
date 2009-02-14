ad9eb7f2d14669697d4e9d9c2c00371131927deb
Matteo Collina <matteo@orsetto>
2009-02-14 18:46:30 +0100

    * Added initial validations to Article.  * Added errros handling classes in the CSS.

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -4,13 +4,38 @@ describe Article do
   before(:each) do
     @valid_attributes = {
       :title => "value for title",
-      :content => "value for content",
-      :updated_at => Time.now,
-      :created_at => Time.now
+      :content => "value for content"
     }
   end
 
   it "should create a new instance given valid attributes" do
     Article.create!(@valid_attributes)
+  end
+
+  it "should set created_at field after creation" do
+    current = Time.now
+    article = Article.create!(@valid_attributes)
+    article.created_at.should > current
+  end
+
+  it "should update the updated_at field after any modification" do
+    creation_time = Time.now
+    article = Article.create!(@valid_attributes)
+    article.updated_at.should > creation_time
+
+    update_time = Time.now
+    article.title = "hello world"
+    article.save!
+    article.updated_at.should > update_time
+  end
+
+  it "should validate the presence of the title" do
+    article = Article.new(:content => "hello world")
+    article.save.should be_false
+  end
+
+  it "should validate the presence of the content" do
+    article = Article.new(:title => "hello world")
+    article.save.should be_false
   end
 end
