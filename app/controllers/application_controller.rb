c3be74fb0649f1b5641c3cd5c7d102c12e35c766
Matteo Collina <matteo@orsetto>
2009-03-02 16:00:04 +0100

    * Called the ThreadLocals.destroy method in a global :after_filter.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,11 @@ class ApplicationController < ActionController::Base
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
+
+  after_filter :destroy_thread_locals
+
+  private
+  def destroy_thread_locals
+    ThreadLocals.destroy
+  end
 end
