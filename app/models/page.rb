5463840e735a71ef50aac9e77e824830449a6786
Matteo Collina <matteo@orsetto>
2009-03-03 17:44:08 +0100

    * Fixed a failing spec for pages/show.html.erb.  * Added consitency check for Page#title and Page#content.

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,4 @@
 class Page < ActiveRecord::Base
+  validates_presence_of :title, :content
+  validates_uniqueness_of :title
 end
--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -4,12 +4,39 @@ describe Page do
   before(:each) do
     @valid_attributes = {
       :title => "value for title",
-      :content => "value for content",
-      :updated_at => Time.now
+      :content => "value for content"
     }
   end
 
   it "should create a new instance given valid attributes" do
     Page.create!(@valid_attributes)
+  end
+
+  it "should update the updated_at field after any modification" do
+    creation_time = Time.now
+    page = Page.create!(@valid_attributes)
+    page.updated_at.should > creation_time
+
+    update_time = Time.now
+    page.title = "hello world"
+    page.save!
+    page.updated_at.should > update_time
+  end
+
+  it "should validate the presence of the title" do
+    page = Page.new(:content => "hello world")
+    page.save.should be_false
+  end
+
+  it "should validate the presence of the content" do
+    page = Page.new(:title => "hello world")
+    page.save.should be_false
+  end
+
+  it "should have a unique title" do
+    Page.create!(@valid_attributes)
+    lambda {
+      Page.create!(@valid_attributes)
+    }.should raise_error(ActiveRecord::RecordInvalid)
   end
 end
