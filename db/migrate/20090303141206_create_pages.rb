4d6ca8365045908a451c9a69e1899b580dbe250e
Matteo Collina <matteo@orsetto>
2009-03-03 15:38:01 +0100

    * Created the page scaffold.  * Integrated tinymce in the page scaffold.  * Integrated the navigations in the page scaffold.

---
+++ db/migrate/20090303141206_create_pages.rb
@@ -0,0 +1,13 @@
+class CreatePages < ActiveRecord::Migration
+  def self.up
+    create_table :pages do |t|
+      t.string :title
+      t.text :content
+      t.timestamp :updated_at
+    end
+  end
+
+  def self.down
+    drop_table :pages
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -9,13 +9,19 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090213164926) do
+ActiveRecord::Schema.define(:version => 20090303141206) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
     t.text     "content"
     t.datetime "updated_at"
     t.datetime "created_at"
+  end
+
+  create_table "pages", :force => true do |t|
+    t.string   "title"
+    t.text     "content"
+    t.datetime "updated_at"
   end
 
 end
