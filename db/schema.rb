dc026bfb6768acaf1d28758b76177a4ab7e946e3
Matteo <matteo@orsetto>
2009-03-29 15:56:17 +0200

    * Added dependency to Authlogic.  * Generated the User scaffold.  * Generated the UserSession model.  * Generated the UserSessions controller.  * Applied needed migration.

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090303141206) do
+ActiveRecord::Schema.define(:version => 20090329133844) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -23,6 +23,23 @@ ActiveRecord::Schema.define(:version => 20090303141206) do
     t.text     "content"
     t.datetime "updated_at"
     t.integer  "position"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "login",                             :null => false
+    t.string   "email",                             :null => false
+    t.string   "crypted_password",                  :null => false
+    t.string   "password_salt",                     :null => false
+    t.string   "persistence_token",                 :null => false
+    t.integer  "login_count",        :default => 0, :null => false
+    t.integer  "failed_login_count", :default => 0, :null => false
+    t.datetime "last_request_at"
+    t.datetime "current_login_at"
+    t.datetime "last_login_at"
+    t.string   "current_login_ip"
+    t.string   "last_login_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
---
+++ spec/views/users/edit.html.erb_spec.rb
@@ -0,0 +1,26 @@
+require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
+
+describe "/users/edit.html.erb" do
+  include UsersHelper
+  
+  before(:each) do
+    assigns[:user] = @user = stub_model(User,
+      :new_record? => false,
+      :login => "value for login",
+      :email => "value for email",
+      :password => "value for password"
+    )
+  end
+
+  it "renders the edit user form" do
+    render
+    
+    response.should have_tag("form[action=#{user_path(@user)}][method=post]") do
+      with_tag('input#user_login[name=?]', "user[login]")
+      with_tag('input#user_email[name=?]', "user[email]")
+      with_tag('input#user_password[name=?]', "user[password]")
+    end
+  end
+end
+
+
