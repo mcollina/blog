e675a3f79998fd4912ddff6a7bed69f020252b3f
Matteo Collina <matteo.collina@gmail.com>
2009-08-03 11:48:21 +0200

    Updated main navigations.

--- config/initializers/navigations.rb
+++ config/initializers/navigations.rb
@@ -9,4 +9,10 @@ Navigations::Repository.get(:main) do |nav|
   nav.page_factory do
     Page.find(:all)
   end
+
+  nav.page do |page|
+    # TODO this navigation should be made visible only if an admin is logged in.
+    page.name = "New Page"
+    page.link_to_eval = "new_page_path"
+  end
 end
